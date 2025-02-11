9e5e506205fd8ec5d690c8f7a23a1baf09b23c5d
Dmitry223 <34372961+Dmitry223@users.noreply.github.com>
2025-02-11 14:09:36 +0400

    Fastex index.js
    Added 2 more iron network wallets

--- projects/fastex/index.js
+++ projects/fastex/index.js
@@ -14,6 +14,8 @@ const config = {
   tron: {
     owners: [
         'TPj7TCJ9rxdd243yQ3tc7iJzqcEYtupB4v'
+        'TXW8f2umgDJhVarwosuGW1d8Wr4FaPpAEb'
+        'TDwRF28KJQhcGV46yRDFXgFdcLbztjxVbs'
     ]
   },
 }
